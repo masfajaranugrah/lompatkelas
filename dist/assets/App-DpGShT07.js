7812e87ac18924e1d7e0cb182a630820847876b7
masfajaranugrah <massfajar223@gamil.com>
2024-09-22 22:45:13 +0700

    egc

---
+++ dist/assets/App-DpGShT07.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-09s7Jgvw.js","assets/index-BRBIgKMi.js","assets/index-BjnDRNuH.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o,j as t}from"./index-BRBIgKMi.js";const s=e.lazy(()=>o(()=>import("./index-09s7Jgvw.js").then(r=>r.i),__vite__mapDeps([0,1,2])));function n(){return t.jsx(t.Fragment,{children:t.jsx(s,{})})}export{n as default};
